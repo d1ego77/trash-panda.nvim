0c490ebc852c530d7737bf3091973d4aefc1cd48
Diego Montoya Castro <markii@mac.local>
2024-08-17 16:36:06 -0600

    test color hint

--- colors/alabaster.lua
+++ colors/alabaster.lua
@@ -583,7 +583,7 @@ else
         DiagnosticInfo = { fg = info },
         DiagnosticVirtualTextError = { bg = "#EAEAEA", fg = error },
         DiagnosticVirtualTextWarn = { bg = "#EAEAEA", fg = warn },
-        DiagnosticVirtualTextHint = { bg = "#EAEAEA", fg = "#0F171D" },
+        DiagnosticVirtualTextHint = { bg = "#EAEAEA", fg = "#C3D0DA" },
         DiagnosticVirtualTextInfo = { bg = "#ADFFB7", fg = "#042F09" },
 
         --- Treesitter
