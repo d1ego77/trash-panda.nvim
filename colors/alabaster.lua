c7f84f9b5a2ebbacdf76e87833e2499efa76840c
Diego Montoya Castro <markii@mac.local>
2024-08-17 15:47:29 -0600

    testing colors

--- colors/alabaster.lua
+++ colors/alabaster.lua
@@ -582,7 +582,7 @@ else
         DiagnosticHint = { fg = hint },
         DiagnosticInfo = { fg = info },
         DiagnosticVirtualTextError = { bg = "#F8B28F", fg = "#411414" },
-        DiagnosticVirtualTextWarn = { bg = "#a6a6a6", fg = "#F0AD4E" },
+        DiagnosticVirtualTextWarn = { bg = "#0e1415", fg = "#f4b371" },
         DiagnosticVirtualTextHint = { fg = "#0F171D", bg = "#C3D0DA" },
         DiagnosticVirtualTextInfo = { bg = "#ADFFB7", fg = "#042F09" },
 
