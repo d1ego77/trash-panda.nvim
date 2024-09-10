edbc66f94fb6aaec8ebe4099b8797b685c73e7af
Diego <dalonso77@gmail.com>
2024-09-10 16:00:58 -0600

    test

--- colors/trash-panda.lua
+++ colors/trash-panda.lua
@@ -420,7 +420,7 @@ else
     local const_fg = "#B4340B"
     local active = "#ffbc5d"
     local active_blue = "#007acc"
-    local string_fg = "#448c27"
+    local string_fg = "#c41a16"
     local darker_fg = "#7d7d7d"
     local diffadd = "#284918"
     local diffdelete = "#B40600"
